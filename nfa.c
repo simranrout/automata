0a302ce0d6d6f9b28b7acf81bba9f984e7d35714
simranrout <43809488+simranrout@users.noreply.github.com>
2019-03-20 21:48:35 +0530

    Update and rename nfa1.c to nfa.c

--- nfa.c
+++ nfa.c
@@ -1,24 +1,15 @@
-
+//accpecting a string by nfa
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 void compute(int,int);
-//FILE *fp,*fp1,*fp2;
 char str[40];
 int temp[30];
-
 int Q[]={0,1,2};
 int E[]={0,1};
 int in_st=0;
 int f[]={2};
 int Rules[3][2][3]={{{0,-1,-1},{0,1,-1}},{{-1,-1,2},{-1,-1,2}},{{-1,-1,-1},{-1,-1,-1}}};
-//fp1=fopen("INPUT.txt","r");
-/*if(fp1==NULL)
-{
-printf("could not open file\n");
-exit(0);
-}*/
-//fp2=fopen("OUTPUT1.txt","w");
 main()
 {
 	int i=0;
@@ -29,12 +20,6 @@ main()
 	{
 		compute(c_st,i);
 	}
-
-		
-	
-	
-//fclose(fp1);
-//fclose(fp2);
 }
 void compute(int c_st,int i)
 {
